3297627cffcfdde6903823b5d90dbd9358beeca4
karl <thefalconsok@yahoo.co.uk>
2020-06-08 15:40:05 +0100

    mover script and nav mesh annded

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [DisallowMultipleComponent]
 public class Mover : MonoBehaviour
-
+{
     //config
     [SerializeField] Transform target;
-{
-    
+    private void Start()
+    {
+        // print the path to the temporary data folder
+        print(Application.temporaryCachePath)
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
