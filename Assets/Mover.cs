628fb895ef88a8b5e00872ad8c19adfedfb926ae
karl <thefalconsok@yahoo.co.uk>
2020-06-08 12:53:50 +0100

    Terrain textured and details added

---
+++ Assets/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Mover : MonoBehaviour
+
+    //config
+    [SerializeField] Transform target;
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
