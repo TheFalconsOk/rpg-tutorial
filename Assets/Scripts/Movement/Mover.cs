34bbe33084e331f955f5d8d188074d0caeb2bbb7
karl <thefalconsok@yahoo.co.uk>
2020-06-16 20:47:43 +0100

    asset pack import and folder structure

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[DisallowMultipleComponent]
+public class Mover : MonoBehaviour
+{
+    //config
+    [SerializeField] Transform target;
+    public NavMeshAgent agent;
+    Ray lastray;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        UpdateAnimator();
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }  
+    }
+
+    private void MoveToCursor()
+    {
+        //sets ray
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //sets ray hit
+        RaycastHit hit;
+        //sets hasHit flag
+        bool hasHit = Physics.Raycast(ray, out hit);
+        //if hit sent destination point
+        if (hasHit)
+        {
+            agent.destination = hit.point;
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+}
