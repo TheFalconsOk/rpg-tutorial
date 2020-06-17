4d029acc4b7c7c420652c35d16fb95d4495fdb48
karl <thefalconsok@yahoo.co.uk>
2020-06-17 16:22:03 +0100

    prevent movement being called on combat target click

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
+using System;
 
 namespace RPG.Control
 {
@@ -11,25 +13,54 @@ namespace RPG.Control
         // Start is called before the first frame update
         void Update()
         {
+            if (InteractWithCombat()) return;
+            if (InteractWithMovement()) return;
+            print("nothing to do");
+        }
+
+        private bool InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach(RaycastHit hit in hits)
+            {
+                var clickTarget = hit.transform.GetComponent<CombatTarget>();
+
+                if (clickTarget == null) continue;
+                else if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack();
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private bool InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
+                return true;
             }
+            return false;
         }
 
         private void MoveToCursor()
         {
-            //sets ray
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             //sets ray hit
             RaycastHit hit;
             //sets hasHit flag
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             //if hit sent destination point
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
